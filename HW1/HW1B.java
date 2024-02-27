29e1fbe08cba81ce8f62e1295f841ac4105f7376
Nguyen Van Huy <23020379@vnu.edu.vn>
2024-02-27 19:29:45 +0700

    first commit

---
+++ HW1/HW1B.java
@@ -0,0 +1,10 @@
+public class HW1B {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn Văn Huy\t"+"23020379\t"+"K68AI1\t"+"huyvanzzz\t"+"23020379@vnu.edu.vn");
+        for(int i=9;i>0;i--){
+            System.out.println(i+ " bottles of beer on the wall, " +i+ " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            if(i==1){System.out.println("No more bottles of beer on the wall.");}
+        }
+    }
+}
