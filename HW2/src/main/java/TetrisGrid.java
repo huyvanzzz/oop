baa24ac2cff04d72eeb3a04678d234b603aaf862
huyvanzzz <23020379@vnu.edu.vn>
2024-04-05 00:04:53 +0700

    Update TetrisGrid.java

--- HW2/src/main/java/TetrisGrid.java
+++ HW2/src/main/java/TetrisGrid.java
@@ -4,53 +4,50 @@
 
 public class TetrisGrid {
 	private boolean[][] grid;
+
 	/**
 	 * Constructs a new instance with the given grid.
 	 * Does not make a copy.
+	 *
 	 * @param grid
 	 */
 	public TetrisGrid(boolean[][] grid) {
 		this.grid = grid;
 	}
-	
-	
+
 	/**
 	 * Does row-clearing on the grid (see handout).
 	 */
 	public void clearRows() {
-		// kích thước hàng
-		int kthang = grid.length;
-		int ktcot = grid[0].length;
-		boolean[] fullkthang = new boolean[kthang];
-		for(int i=0;i<kthang;i++){
-			boolean a = true;
-			for(int j=0;j<ktcot;j++){
-				if(!grid[i][j]){
-                   a =false;
-				   break;
+		int rows = grid.length;
+		int cols = grid[0].length;
+		int[] colsToClear = new int[cols];
+		int colsToClearCount = 0;
+		for (int i = 0; i < cols; i++) {
+			boolean isColFull = true;
+			for (int j = 0; j < rows; j++) {
+				if (!grid[j][i]) {
+					isColFull = false;
+					break;
 				}
 			}
-			fullkthang[i]= a;
-		}
-		int z =0;
-		for (int i=kthang-1;i>=0;i--){
-			if(fullkthang[i]){
-				z++; // tăng số lần dịch
-			} else if (z>0) {
-				for(int j=0;j<ktcot;j++){
-					grid[i+z][j]=grid[i][j];
-				}
+			if (isColFull) {
+				colsToClear[colsToClearCount++] = i;
 			}
 		}
-		for(int i=0;i<z;i++){
-			for(int j=0;j<ktcot;j++){
-				grid[i][j]=false;
+		for (int i = 0; i < colsToClearCount; i++) {
+			for (int j = 0; j < rows; j++) {
+				for (int k = colsToClear[i]; k < cols - 1; k++) {
+					grid[j][k] = grid[j][k + 1];
+				}
+				grid[j][cols - 1] = false;
 			}
 		}
 	}
-	
+
 	/**
 	 * Returns the internal 2d grid array.
+	 *
 	 * @return 2d grid array
 	 */
 	boolean[][] getGrid() {
