0cfdc92a56297b482bd084c00fd3afff75417e11
huyvanzzz <23020379@vnu.edu.vn>
2024-04-04 23:56:47 +0700

    first commit

---
+++ HW1/src/ALLEqual.java
@@ -0,0 +1,15 @@
+public class ALLEqual {
+    public static void main(String[] args) {
+        int a = Integer.parseInt("2");
+        int b = Integer.parseInt("1");
+        int c = Integer.parseInt("0");
+
+        if (a == b && a == c) {
+            System.out.println("all equal");
+        }
+        else {
+            System.out.println("not all equal");
+        }
+
+    }
+}
---
+++ HW1/src/UseThree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UseThree {
+
+    public static void main(String[] args) {
+        String c,b,a;
+        Scanner sc =new Scanner(System.in);
+        a=sc.nextLine();
+        b=sc.nextLine();
+        c=sc.nextLine();
+        System.out.print("Hi, ");
+        System.out.print(c);
+        System.out.print(", ");
+        System.out.print(b);
+        System.out.print(", and ");
+        System.out.print(a);
+        System.out.println(".");
+    }
+
+}
